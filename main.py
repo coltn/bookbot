28e48841409ff97cd00f667e6d7d7f2457302436
coltn <mail@coltn.io>
2025-03-18 16:34:14 -0600

    finalize program

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def main():
     chars.sort(reverse=True, key=stats.sort_on_count)
 
     print(
-        "============ BOOKBOT ============\n\
-Analyzing book found at books/frankenstein.txt...\n\
+        f"============ BOOKBOT ============\n\
+Analyzing book found at {filepath}...\n\
 ----------- Word Count ----------"
     )
     print(f"Found {words} total words")
